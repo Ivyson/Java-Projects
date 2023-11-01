38b376c2c7de9c5d0e48f192628b07772ab0b5b8
ivykiid@gmail.com <you@example.com>
2023-11-01 11:58:18 +0200

    MainInit

---
+++ JavaRevisionProject/src/javarevisionproject/JavaRevisionProject.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javarevisionproject;
+
+/**
+ *
+ * @author Sam
+ */
+public class JavaRevisionProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
