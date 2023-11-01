99ac60826e5063244ed0c3c14d59303e22afbb24
230500226 <230500226@mycput.ac.za>
2023-11-01 12:15:33 +0200

    NetBeans16change

--- JavaRevisionProject/src/javarevisionproject/JavaRevisionProject.java
+++ JavaRevisionProject/src/javarevisionproject/JavaRevisionProject.java
@@ -6,15 +6,15 @@ package javarevisionproject;
 
 /**
  *
- * @author Sam
+ * @author ldxt460s
  */
 public class JavaRevisionProject {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        /**
+         * @param args the command line arguments
+         */
+        public static void main(String[] args) {
+                // TODO code application logic here
+        }
+        
 }
