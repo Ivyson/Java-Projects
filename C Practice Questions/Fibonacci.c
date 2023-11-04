82de22dc514d20c2209ba8c8aab937565f0d922e
82Sam <53128739+Ivyson@users.noreply.github.com>
2023-11-04 23:07:22 +0000

    C Practice Questions

--- C Practice Questions/Fibonacci.c
+++ C Practice Questions/Fibonacci.c
@@ -3,7 +3,17 @@
 int main()
 {
     int first_num,second_number,last_number,num_terms;
+    last_number = 0;
     printf("How many Numbers must your sequence have?");
     scanf("%d",&num_terms);
-    for()
+    printf("What is your first value:");
+    scanf("%d",&first_num);
+    for(int i = 0; i < num_terms; i++)
+    {
+        second_number = first_num + last_number;
+        first_num = last_number;
+        last_number = second_number;
+        printf("%d\t",second_number);
+        
+    }
 }
