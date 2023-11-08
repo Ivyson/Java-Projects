afb79c4c1bc0e6ba7df286c3a0ddf37e4603dfc6
ivykiid@gmail.com <you@example.com>
2023-11-08 23:27:13 +0200

    hi

---
+++ IconsExperiment/src/iconsexperiment/IconsExperiment.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package iconsexperiment;
+
+import Swing.LogIn;
+
+/**
+ *
+ * @author Sam
+ */
+public class IconsExperiment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new LogIn().setVisible(true);
+    }
+    
+}
