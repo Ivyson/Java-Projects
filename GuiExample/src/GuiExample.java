bcb4bfd463969389e389dd20d4c1f2aee7948a0b
82Sam <53128739+Ivyson@users.noreply.github.com>
2023-11-16 11:33:24 +0200

    Update 16 November

--- GuiExample/src/GuiExample.java
+++
@@ -1,9 +0,0 @@
-public class GuiExample{
-
-    public static void main(String[] args) {
-        System.out.println("Hello M People");
-        StopWatch1 watch = new StopWatch1();
-        watch.setVisible(true);
-       
-    }
-}
