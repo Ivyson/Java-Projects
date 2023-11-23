d100a4d890992632ea94f3731ff67f72cfff7d35
82Sam <53128739+Ivyson@users.noreply.github.com>
2023-11-23 12:28:16 +0200

    Progress 23 November 12:30

---
+++ Vs code java practices/Updated Calculator/src/MatrixMultiplication.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+public class MatrixMultiplication {
+    Scanner scanner;
+    public MatrixMultiplication()
+    {
+
+    }
+    //Trying to multiply two matrices
+    public void Matrice()
+    {
+        scanner = new Scanner(System.in);
+        int row1,column1,row2,column2,Option;
+        int[][] Matrice1,Matrice2,result;
+        System.out.println("How many rows does your first Matrice has?");
+        row1 = scanner.nextInt();
+        scanner.nextLine(); //To consume the new line created after the integer scanning
+        System.out.println("How many columns does your first Matrice has?");
+        column1 = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("How many rows does your second Matrice has?");
+        row2 = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("How many columns does your second Matrice has?");
+        column2 = scanner.nextInt();
+        Matrice1 = new int[row1][column1];
+        Matrice2 = new int[row2][column2];
+        System.out.println("Please enter the data for the first matrice:");
+        for(int i = 0; i < row1; i++)
+        {
+            for(int j = 0; j < column1; j++)
+            {
+                Matrice1[i][j] = scanner.nextInt();
+                scanner.nextLine(); //To ensure that the new line created is destroyed
+            }
+        }
+        System.out.println("Please enter the data for the second matrice:");
+         for(int i = 0; i < row2; i++)
+        {
+            for(int j = 0; j < column2; j++)
+            {
+                Matrice2[i][j] = scanner.nextInt();
+                scanner.nextLine(); //To ensure that the new line created is destroyed
+            }
+        }
+        System.out.println("What Matrix operation do you want to perform:\n1.Addition\n2.Multiplication");
+        Option = scanner.nextInt();
+        scanner.nextLine();
+        switch(Option)
+        {
+            case 1:
+                if(row1 == row2 && column1 == column2)
+                {
+                    result = new int[row1][column1];
+                    for(int i = 0; i < row1; i++)
+                    {
+                        for(int j = 0; j < column1; j++)
+                        {
+                            result[i][j] = Matrice1[i][j] + Matrice2[i][j];
+                        }
+                    }
+
+                     for(int i = 0; i < row1; i++)
+                    {
+                        for(int j = 0; j < column1; j++)
+                        {
+                            System.out.printf("%d\t",result[i][j]);
+                        }
+                        System.out.println();
+                    }
+                    
+                }
+                else{
+                    System.out.println("The sizes of the two matrices has to be equal for addition to be possible");
+                }
+                break;
+            case 2: //Multiplying matrices
+                
+                break;
+            default:
+                System.out.println("The option you chose might've be wrong!");
+        }
+        
+    }
+}
--- Vs code java practices/Updated Calculator/src/PracticeJava.java
+++ Vs code java practices/Updated Calculator/src/PracticeJava.java
@@ -1,9 +1,13 @@
 import javax.swing.JOptionPane;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.*; //Import the scanner Library just in case i will need to use a scanner instead of casting the value from a JOptionPane
 //Use math library that has maths methods in it to perform complex maths operations like complex numbers and all those things
 
 public class PracticeJava {
     double result1;
+    Scanner scanner;
+    public final Double PI = 3.141592654;
 
     public double adding(double num1, double num2) {
         return (num1 + num2);
@@ -35,13 +39,16 @@ public class PracticeJava {
         return fact;
     }
 
-    public double root(double num1, double num2) {
+    public void root(double num1, double num2) {
         if (num2 == 0) {
             JOptionPane.showMessageDialog(null, "The number on the power cannot be zero");
         } else if (num1 < 0) {
             // Handle negative number case if needed
+            System.out.println("The "+num2+" Root of "+num1+" is : "+Math.pow(Math.abs(num1), 1/num2)+"i");
         }
-        return (Math.pow(num1, 1 / num2));
+        else{
+            JOptionPane.showMessageDialog(null, "The "+num2+" Root of "+num1+" is : "+Math.pow(Math.abs(num1), 1/num2));
+        }
     }
 
     public double Trigonometry(double num1, int Option2) {
@@ -108,18 +115,275 @@ public class PracticeJava {
         double first_term = Double.parseDouble(JOptionPane.showInputDialog("What is your first term"));
         double comm_diff = Double.parseDouble(JOptionPane.showInputDialog("What is the common difference"));
         double sum = num_terms * (2 * first_term + (num_terms - 1) * comm_diff) / 2;
+        Double tn;
         for (int i = 1; i <= num_terms; i++) {
-            double tn = 2 * first_term + (num_terms - 1) * comm_diff;
-            System.out.printf("%lf\t", tn);
+            tn = first_term + (i - 1) * comm_diff;
+            System.out.printf("%.0f\t", tn);
         }
+        System.err.println();
         System.out.println("The sum of all the terms in this series is " + sum);
     }
-     //Implement a case for Fibonacci sequence
-    //Finding the factors of a number
+    //Finding factors of a number
+    public void numberfactor(int num)
+    {
+        int j = 0;
+        Integer[] factors;
+        //Integer index = num;
+        Integer[] arr = new Integer[num];
+        for(int i = 1; i <= num; i++) 
+        {
+            if(num % i == 0)
+            {
+                arr[j] = i;
+                System.out.println(arr[j]);
+                j++;
+                
+            }
+        } 
+        
+        factors = Arrays.copyOf(arr, j);// This is to copy and use only the critical values stored and not all the values stored in arr
+        JOptionPane.showMessageDialog(null,Arrays.toString(factors));  
+    }
     //Finding factors of a quadratic formula
+    public void Quadratic()
+    {
+        Double secPower = Double.parseDouble(JOptionPane.showInputDialog("What is the value of the highest power(x^2)"));
+        Double bValue = Double.parseDouble(JOptionPane.showInputDialog("Enter the value of b,The coefficient of x"));
+        Double constant = Double.parseDouble(JOptionPane.showInputDialog("Enter the value constant/The value that has no x in it"));
+        Double disciminant = bValue*bValue -4*secPower*constant;
+        Double x1,x2;
+        if(disciminant < 0)
+        {
+            JOptionPane.showMessageDialog(null, "This function has factors that belong in the complex number plane");
+        }
+        else{
+            x1 = -(bValue/2) + Math.sqrt(disciminant)/2;
+            x2 = -(bValue/2) - Math.sqrt(disciminant)/2;
+            JOptionPane.showMessageDialog(null, "The x- intercepts are :"+x1+"; "+x2+ "\n The y-intercept :"+constant);
+            System.exit(0);
+        }
+        JOptionPane.showMessageDialog(null, "The Y-intercept of the graph is: "+constant);
+    }
+    //Representing numbers in the Scientific Notation
+    public void ScForm()
+    {
+        int ScNum = Integer.parseInt(JOptionPane.showInputDialog("Enter the number"));
+        if(ScNum == 0)
+        {
+            JOptionPane.showMessageDialog(null, "0 in Scientific notation can be written as 0.00E0");
+        }
+
+        DecimalFormatSymbols symbol = new DecimalFormatSymbols();
+        symbol.setDecimalSeparator('.');
+        symbol.setExponentSeparator("E");
+        DecimalFormat df = new DecimalFormat("0.00E0", symbol);
+        JOptionPane.showMessageDialog(null, "The representation of "+ScNum+" is: "+df.format(ScNum));
+    }
+     //Method for Matrices
+     public void matrix()
+     {
+        //Specifically for 2D Matrices
+        Integer[][] matrices;
+        scanner = new Scanner(System.in);
+        int row = Integer.parseInt(JOptionPane.showInputDialog("Your matrix has how many rows"));
+        int columns = Integer.parseInt(JOptionPane.showInputDialog("How many columns does your matrix have"));
+        matrices = new Integer[row][columns];
+        JOptionPane.showMessageDialog(null, "Everything else from here will be done on the console");
+        System.out.println("You will be asked to input the vaalues of in the matrice");
+        for(int i = 0; i < row; i++)
+        {
+        for (int j = 0; j < columns; j++)
+            {
+                System.out.printf("Matrice[%d][%d]=",i+1,j+1);
+                matrices[i][j] = scanner.nextInt();
+                scanner.nextLine(); //To consume the next line character created by a user pressing enter
+            }
+            //System.out.println("\n");
+        }
+        System.out.println("Your matrice will be displayed below!");
+        for(int i = 0; i < row; i++)
+        {
+            System.out.printf("|");
+            for (int j = 0; j < columns; j++)
+            {
+                System.out.printf("%d\t",matrices[i][j]);
+            }
+            System.out.printf("|\n");
+        }
+        int option = Integer.parseInt(JOptionPane.showInputDialog("Which Matrice operation do you want to perfom\n1.Transpose of matrice"));
+        switch (option) {
+            case 1:
+            System.out.println("__________________________");
+            System.out.println("The Transpose of your matrice:");
+                for(int i = 0; i < columns; i++)
+                {
+                    System.out.printf("|");
+                    for(int j = 0; j < row; j++)
+                    {
+                        System.out.printf("%d\t",matrices[j][i]);
+                    }
+                    System.out.printf("|\n");
+                }
+                break;
+            case 2:
+                if(row == columns)
+                {
+
+                }
+                else
+                {
+                    System.out.println("The determinant of a non square matrix is undefined");
+                }
+                break;
+            
+            default:
+                break;
+        }
+
+     }
+     public void complex()
+     {
+        Double Imaginery,real,modulus,argument,ArgDegrees,real2,Imaginery2,real3,Imaginery3,modulus2,modulus3,argument2,argument3,realresult,ImagineryResult;
+        Double modulusresult,argumentResult,realResult;
+         Integer Option;
+        Integer Opt = Integer.parseInt(JOptionPane.showInputDialog("How many complex numbers are we dealing with:\n1.One\n2.Two\3.Three"));
+        switch (Opt) {
+            case 1: //For 1 complex number
+            real = Double.parseDouble(JOptionPane.showInputDialog("Enter the real number"));
+        Imaginery = Double.parseDouble(JOptionPane.showInputDialog("Enter the Imaginery number(without the i/j)"));
+        modulus = Math.sqrt(Imaginery*Imaginery + real*real);
+        //Provide the answer according to the correct quadrant(real >> x; Imaginery >> y)
+        if(real > 0 && Imaginery > 0) //First quadrant
+        {
+            argument = Math.abs(Math.atan(Imaginery/real));// Provided value is in radians
+        }
+        else if(real > 0 && Imaginery < 0) //4th Quadrant
+        {
+            argument = 2*PI - Math.abs(Math.atan(Imaginery/real));
+        }
+        else if(real < 0 && Imaginery < 0) //3rd quadrant 
+        {
+            argument = PI + Math.abs(Math.atan(Imaginery/real));
+        }
+        else { //Second quadrant
+            argument = PI - Math.abs(Math.atan(Imaginery/real));
+        }
+        ArgDegrees = (180/PI)*argument;
+        JOptionPane.showMessageDialog(null, "The arguement of the entered complex numberin radians is "+argument+"\nThe arguement of the entered complex number in Degrees is "+ArgDegrees);
+            break;
+        case 2: //Operating on two complex numbers
+                Option = Integer.parseInt(JOptionPane.showInputDialog("What operation do you want to perform:\n1.Addition\n2.Division\n3.Multiplication\n4.Subtraction"));
+            switch (Option) {
+            case 1:
+                real = Double.parseDouble(JOptionPane.showInputDialog("Enter the real number"));
+                Imaginery = Double.parseDouble(JOptionPane.showInputDialog("Enter the Imaginery number(without the i/j)"));
+                modulus = Math.sqrt(Imaginery * Imaginery + real * real);
+                real2 = Double.parseDouble(JOptionPane.showInputDialog("Enter the real number"));
+                Imaginery2 = Double.parseDouble(JOptionPane.showInputDialog("Enter the Imaginery number(without the i/j)"));
+                modulus2 = Math.sqrt(Imaginery * Imaginery + real * real);
+                break;
+                //Add code and cases for two complex numbers operation
+                default:// For 2 complex numbers
+                break;
+            }       
+        case 3: //For three Complex numbers
+                  Option = Integer.parseInt(JOptionPane.showInputDialog("What operation do you want to perform:\n1.Addition\n2.Division\n3.Multiplication\n4.Subtraction"));
+                  real = Double.parseDouble(JOptionPane.showInputDialog("1.Enter the real number"));
+                Imaginery = Double.parseDouble(JOptionPane.showInputDialog("1.Enter the Imaginery number(without the i/j)"));
+                modulus = Math.sqrt(Imaginery * Imaginery + real * real);
+                real2 = Double.parseDouble(JOptionPane.showInputDialog("2.Enter the real number"));
+                Imaginery2 = Double.parseDouble(JOptionPane.showInputDialog(".2Enter the Imaginery number(without the i/j)"));
+                modulus2 = Math.sqrt(Imaginery * Imaginery + real * real);
+                real3 = Double.parseDouble(JOptionPane.showInputDialog("3.Enter the real number"));
+                Imaginery3 = Double.parseDouble(JOptionPane.showInputDialog("3.Enter the Imaginery number(without the i/j)"));
+                modulus3 = Math.sqrt(Imaginery * Imaginery + real * real);
+                //Provide the answer according to the correct quadrant(real >> x; Imaginery >> y)
+        if(real > 0 && Imaginery > 0) //First quadrant
+        {
+            argument = Math.abs(Math.atan(Imaginery/real));// Provided value is in radians
+        }
+        else if(real > 0 && Imaginery < 0) //4th Quadrant
+        {
+            argument = 2*PI - Math.abs(Math.atan(Imaginery/real));
+        }
+        else if(real < 0 && Imaginery < 0) //3rd quadrant 
+        {
+            argument = PI + Math.abs(Math.atan(Imaginery/real));
+        }
+        else { //Second quadrant
+            argument = PI - Math.abs(Math.atan(Imaginery/real));
+        }
+        //For complex number 2
+        if(real2 > 0 && Imaginery2 > 0) //First quadrant
+        {
+            argument2 = Math.abs(Math.atan(Imaginery2/real2));// Provided value is in radians
+        }
+        else if(real2 > 0 && Imaginery2 < 0) //4th Quadrant
+        {
+            argument2 = 2*PI - Math.abs(Math.atan(Imaginery2/real2));
+        }
+        else if(real2 < 0 && Imaginery2 < 0) //3rd quadrant 
+        {
+            argument2 = PI + Math.abs(Math.atan(Imaginery2/real2));
+        }
+        else { //Second quadrant
+            argument2 = PI - Math.abs(Math.atan(Imaginery2/real2));
+        }
+        //For complex number 3
+        if(real3 > 0 && Imaginery3 > 0) //First quadrant
+        {
+            argument3 = Math.abs(Math.atan(Imaginery3/real3));// Provided value is in radians
+        }
+        else if(real3 > 0 && Imaginery3 < 0) //4th Quadrant
+        {
+            argument3 = 2*PI - Math.abs(Math.atan(Imaginery3/real3));
+        }
+        else if(real3 < 0 && Imaginery3 < 0) //3rd quadrant 
+        {
+            argument3 = PI + Math.abs(Math.atan(Imaginery3/real3));
+        }
+        else { //Second quadrant
+            argument3 = PI - Math.abs(Math.atan(Imaginery3/real3));
+        }
+                
+        switch (Option) {
+            case 1:
+                //Addition of 3 complex numbers
+            realresult = real + real2 + real3;
+            ImagineryResult = Imaginery + Imaginery2 + Imaginery3;
+            JOptionPane.showMessageDialog(null, "The Resulting complex number is "+realresult+"+("+ImagineryResult+")i");
+                break;
+            case 2:
+            //Division of three complex numbers Needs to handle the way of dividing, if we divide all of the 3 complex numbers in one term or ...
+
+            break;
+            case 3:
+            //Multiplication of all these complex numbers
+            modulusresult = modulus*modulus2*modulus3;
+            argumentResult = argument + argument2 + argument3;
+            //System.out.println("The modulus of the resulting complex number "+modulusresult+" and the argument is "+argumentResult);
+            realResult = modulusresult * Math.cos(argumentResult);
+            ImagineryResult = modulusresult * Math.sin(argumentResult);
+            System.out.println("The resulting complex number : "+realResult+"+("+ImagineryResult+" )i");
+            break; //Operation on three complex numbers
+            default: 
+                break;
+        }
+         
+            default:// For number of Complex numbers dealing with.
+                break;
+         }
+       
+    }
+     
+
+    ///*Handle the issue of when the Imaginer number is zero! */
+    //Finding a gradient of a straight line
+    //Finding a first derivative of a quadratic functions and cubic functions,sin,cosine and tan graphs
     //Using an online algorithm, find the more complex functions' critical values like Cubic functions and more
-    //Finding co-ordinates of graphs
     //Finding geometric sigma and and sum to infinity
+    //Area of a triangle - Cone - Parabola - ellipse - cylinder - ellipsoid(surface area) 
+
     //@Auhtor Sam.
 
     public static void main(String[] args) {
@@ -129,13 +393,14 @@ public class PracticeJava {
         int Option1;
         double results;
         try {
-            Option1 = Integer.parseInt(JOptionPane.showInputDialog("What Operation do you want to perform \n1.Multiplication \n2.Addition \n3.Subtraction \n4.Trigonometric functions \n5.Logs \n6.Factorials \n7. Radical signs\n8.Powers\n9.Arithmetic sum"));
+            Option1 = Integer.parseInt(JOptionPane.showInputDialog("What Operation do you want to perform \n1.Multiplication \n2.Addition \n3.Subtraction \n4.Trigonometric functions \n5.Logs \n6.Factorials \n7. Radical signs\n8.Powers\n9.Arithmetic sum\n10.Finding factors of a number\n11.Finding factors of a quadratic formula\n12.More.."));
             switch (Option1) {
                 case 1:
                     num1 = Double.parseDouble(JOptionPane.showInputDialog("Enter the first number"));
                     num2 = Double.parseDouble(JOptionPane.showInputDialog("Enter the Second number"));
                     results = obj.Multiplication(num1, num2);
                     JOptionPane.showMessageDialog(null, "The product is : " + results);
+                   obj.numberfactor(10);
                     break;
                 case 2:
                     num1 = Double.parseDouble(JOptionPane.showInputDialog("Enter the first number"));
@@ -168,8 +433,8 @@ public class PracticeJava {
                 case 7:
                     num1 = Double.parseDouble(JOptionPane.showInputDialog("Enter the first number"));
                     num2 = Double.parseDouble(JOptionPane.showInputDialog("Enter the number on the root (square root has 2 as an argument)"));
-                    results = obj.root(num1, num2);
-                    JOptionPane.showMessageDialog(null, "The root of " + num1 + " is :" + results);
+                    obj.root(num1, num2);
+                    //JOptionPane.showMessageDialog(null, "The root of " + num1 + " is :" + results);
                     break;
                 case 8:
                     num1 = Double.parseDouble(JOptionPane.showInputDialog("Enter the Base "));
@@ -179,6 +444,29 @@ public class PracticeJava {
                     break;
                 case 9:
                     obj.arithmaticseq();
+                    break;
+                case 10:
+                    int number = Integer.parseInt(JOptionPane.showInputDialog("Enter the number"));
+                    obj.numberfactor(number);
+                    break;
+                case 11:
+                    obj.Quadratic();
+                    break;
+                case 12:
+                    int option2 = Integer.parseInt(JOptionPane.showInputDialog("1.Scientific Notation\n2.Matrix\n3.Complex numbers"));
+                    switch (option2) {
+                        case 1:
+                            obj.ScForm();
+                            break;
+                        case 2:
+                            obj.matrix();
+                            break;
+                        case 3:
+                            obj.complex();
+                            break;
+                        default:
+                            break;
+                    }
                     break;
                 default:
                     JOptionPane.showMessageDialog(null, "The Option you entered doesn't exist");
